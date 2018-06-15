065651ddae86a38a08b4768d41b50c7988f69321
A. Tamakov <sashatinkoff@gmail.com>
2018-06-15 11:19:36 +0500

    Clear sample providing dependency into a module

--- app/src/main/java/com/isidroid/a18/MainActivity.kt
+++ app/src/main/java/com/isidroid/a18/MainActivity.kt
@@ -2,22 +2,9 @@ package com.isidroid.a18
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import com.isidroid.utilsmodule.Billing
-import com.isidroid.utilsmodule.DaggerVersionsComponent
-import com.isidroid.utilsmodule.Diagnostics
-import com.isidroid.utilsmodule.VersionsModule
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        val versionsComponent = DaggerVersionsComponent.builder()
-                .versionsModule(VersionsModule().apply {
-                    name = "Superhero"
-                    version = "2.2"
-                })
-                .build()
-        val billing = Billing().apply { versionsComponent.inject(this) }
-        Diagnostics.i(this, "onCreate w/billing=$billing")
     }
 }
--- utilsmodule/src/main/java/com/isidroid/utilsmodule/VersionsComponent.kt
+++
@@ -1,8 +0,0 @@
-package com.isidroid.utilsmodule
-
-import dagger.Component
-
-@Component(modules = [VersionsModule::class])
-interface VersionsComponent {
-    fun inject(billing: Billing)
-}
