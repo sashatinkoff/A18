8ccd560c8045db68e8fc764e6bec205b76c6d7d9
A. Tamakov <sashatinkoff@gmail.com>
2018-06-15 10:58:18 +0500

    Use qualified for the same type in the module

--- app/src/main/java/com/isidroid/a18/MainActivity.kt
+++ app/src/main/java/com/isidroid/a18/MainActivity.kt
@@ -3,22 +3,20 @@ package com.isidroid.a18
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.google.gson.Gson
+import com.isidroid.a18.di.VersionsModule
 import com.isidroid.utilsmodule.Diagnostics
 import javax.inject.Inject
-import javax.inject.Named
 
 class MainActivity : AppCompatActivity() {
-    //    @Inject lateinit var prefs: SharedPreferences
     @Inject lateinit var gson: Gson
-    @field:[Inject Named("version")] lateinit var version: String
-    @field:[Inject Named("name")] lateinit var name: String
+    @field: [Inject VersionsModule.Version] lateinit var version: String
+    @field: [Inject VersionsModule.Name] lateinit var name: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         App.netComponent.inject(this)
-
 
         var message = mutableListOf(version, name)
         var json = gson.toJson(message)
--- app/src/main/java/com/isidroid/a18/di/VersionsModule.kt
+++ app/src/main/java/com/isidroid/a18/di/VersionsModule.kt
@@ -4,16 +4,21 @@ import javax.inject.Named
 
 import dagger.Module
 import dagger.Provides
+import javax.inject.Qualifier
+
 
 @Module
 class VersionsModule {
-    @Provides @Named("version")
+    @Provides @Version
     fun provideVersion(): String {
         return "1.0.0"
     }
 
-    @Provides @Named("name")
+    @Provides @Name
     fun provideName(): String {
         return "Sasha"
     }
+
+    @Qualifier @Retention(AnnotationRetention.RUNTIME) annotation class Version
+    @Qualifier @Retention(AnnotationRetention.RUNTIME) annotation class Name
 }
