57466d5516ce6ee3f15f4665d4989888e49d1dcf
A. Tamakov <sashatinkoff@gmail.com>
2018-06-21 16:13:42 +0500

    Revert "ImageHeader"
    This reverts commit 76370cf1b55f153eff597171fc4c6d7e6544d302.

--- utilsmodule/src/main/java/com/isidroid/utilsmodule/BaseActivity.kt
+++ utilsmodule/src/main/java/com/isidroid/utilsmodule/BaseActivity.kt
@@ -1,11 +1,10 @@
 package com.isidroid.utilsmodule
 
 import android.support.v7.app.AppCompatActivity
-import com.isidroid.utilsmodule.views.Utils
 
 abstract class BaseActivity : AppCompatActivity() {
     override fun onPause() {
         super.onPause()
-        Utils.hideSoftKeyboard(this)
+        ViewsUtils.hideSoftKeyboard(this)
     }
 }
