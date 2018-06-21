76370cf1b55f153eff597171fc4c6d7e6544d302
A. Tamakov <sashatinkoff@gmail.com>
2018-06-21 16:13:28 +0500

    ImageHeader

--- utilsmodule/src/main/java/com/isidroid/utilsmodule/BaseActivity.kt
+++ utilsmodule/src/main/java/com/isidroid/utilsmodule/BaseActivity.kt
@@ -1,10 +1,11 @@
 package com.isidroid.utilsmodule
 
 import android.support.v7.app.AppCompatActivity
+import com.isidroid.utilsmodule.views.Utils
 
 abstract class BaseActivity : AppCompatActivity() {
     override fun onPause() {
         super.onPause()
-        ViewsUtils.hideSoftKeyboard(this)
+        Utils.hideSoftKeyboard(this)
     }
 }
