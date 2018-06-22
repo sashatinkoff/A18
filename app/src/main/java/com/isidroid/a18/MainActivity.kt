a4a3c6ea25f5ae8a9ead46dbdd5be46423b05e0b
A. Tamakov <sashatinkoff@gmail.com>
2018-06-22 12:27:16 +0500

    Sample of passing en email address to intent with multiple attachments

--- app/src/main/java/com/isidroid/a18/MainActivity.kt
+++ app/src/main/java/com/isidroid/a18/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.isidroid.a18
 
+import android.content.Intent
 import android.os.Bundle
-import com.isidroid.a18.R.id.button
 import com.isidroid.loggermodule.Diagnostics
 import com.isidroid.utilsmodule.BaseActivity
 import io.reactivex.Flowable
@@ -9,8 +9,6 @@ import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_main.*
 import timber.log.Timber
-import java.text.SimpleDateFormat
-import java.util.*
 import java.util.concurrent.TimeUnit
 
 class MainActivity : BaseActivity() {
@@ -18,13 +16,13 @@ class MainActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
         Flowable.interval(1, TimeUnit.SECONDS)
                 .take(10)
-                .subscribe { Timber.e("Take it easy $it") }
+//                .subscribe { Timber.e("Take it easy $it") }
 
         button.setOnClickListener { execute2() }
     }
+
 
     private var position = 0
     private fun execute2() {
@@ -36,7 +34,8 @@ class MainActivity : BaseActivity() {
             2 -> shareLogs()
             3 -> Diagnostics.instance.cancel()
             4 -> shareLogs()
-            5 -> {}
+            5 -> {
+            }
             6 -> Diagnostics.instance.startInfo("Custom logs")
             7 -> Diagnostics.instance.stopInfo("Custom logs")
             8 -> shareLogs()
@@ -49,7 +48,14 @@ class MainActivity : BaseActivity() {
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe {
-                    startActivity(it)
+                    it?.apply {
+                        flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                        putExtra(Intent.EXTRA_EMAIL, arrayOf("sashatinkoff@gmail.com"))
+                        putExtra(Intent.EXTRA_SUBJECT, "Debug info ${BuildConfig.APPLICATION_ID}")
+                        type = "message/rfc822"
+
+                        startActivity(this)
+                    }
                 }
     }
 
