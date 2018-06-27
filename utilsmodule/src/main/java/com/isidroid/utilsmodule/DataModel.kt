d0060899e9852e24a90a5edeed285883a9d70378
A. Tamakov <sashatinkoff@gmail.com>
2018-06-27 13:00:19 +0500

    Base data model

--- utilsmodule/src/main/java/com/isidroid/utilsmodule/DataModel.kt
+++ utilsmodule/src/main/java/com/isidroid/utilsmodule/DataModel.kt
@@ -2,14 +2,26 @@ package com.isidroid.a18.data
 
 import io.realm.Realm
 import io.realm.RealmModel
+import io.realm.kotlin.deleteFromRealm
 
 interface DataModel : RealmModel {
     fun save() {
-        Realm.getDefaultInstance().apply {
-            beginTransaction()
-            insertOrUpdate(this@DataModel)
-            commitTransaction()
+        realmExe { it.insertOrUpdate(this) }
+    }
+
+    fun delete() {
+        realmExe { deleteFromRealm() }
+    }
+
+    companion object {
+        fun realmExe(execute: (realm: Realm) -> Unit) {
+            Realm.getDefaultInstance().apply {
+                beginTransaction()
+                execute(this)
+                commitTransaction()
+            }
         }
     }
 }
+
 
