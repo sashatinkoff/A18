5a0099f3b231d3d77b92e572bc40a5b720356f2f
A. Tamakov <sashatinkoff@gmail.com>
2018-07-10 13:13:03 +0500

    1.0.13

--- utilsmodule/src/main/java/com/isidroid/utilsmodule/Outcome.kt
+++ utilsmodule/src/main/java/com/isidroid/utilsmodule/Outcome.kt
@@ -1,10 +1,5 @@
 package com.isidroid.utilsmodule
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MediatorLiveData
-import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.subjects.PublishSubject
-
 sealed class Outcome<T> {
     data class Progress<T>(var loading: Boolean) : Outcome<T>()
     data class Success<T>(var data: T) : Outcome<T>()
@@ -17,40 +12,4 @@ sealed class Outcome<T> {
 
         fun <T> failure(e: Throwable): Outcome<T> = Failure(e)
     }
-}
-
-/**
- * Extension function to convert a Publish subject into a LiveData by subscribing to it.
- **/
-fun <T> PublishSubject<T>.toLiveData(compositeDisposable: CompositeDisposable): LiveData<T> {
-    val data = MediatorLiveData<T>()
-    compositeDisposable.add(this.subscribe { t: T -> data.value = t })
-    return data
-}
-
-/**
- * Extension function to push a failed event with an exception to the observing outcome
- * */
-fun <T> PublishSubject<Outcome<T>>.failed(e: Throwable) {
-    with(this){
-        loading(false)
-        onNext(com.isidroid.utilsmodule.Outcome.Companion.failure(e))
-    }
-}
-
-/**
- * Extension function to push  a success event with data to the observing outcome
- * */
-fun <T> PublishSubject<Outcome<T>>.success(t: T) {
-    with(this){
-        loading(false)
-        onNext(com.isidroid.utilsmodule.Outcome.Companion.success(t))
-    }
-}
-
-/**
- * Extension function to push the loading status to the observing outcome
- * */
-fun <T> PublishSubject<Outcome<T>>.loading(isLoading: Boolean) {
-    this.onNext(com.isidroid.utilsmodule.Outcome.Companion.loading(isLoading))
 }
