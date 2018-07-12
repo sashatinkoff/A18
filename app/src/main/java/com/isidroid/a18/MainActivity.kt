c240afceb885669b8249b8a0a532624d9127c736
A. Tamakov <sashatinkoff@gmail.com>
2018-07-12 15:36:17 +0500

    1.0.18

--- app/src/main/java/com/isidroid/a18/MainActivity.kt
+++ app/src/main/java/com/isidroid/a18/MainActivity.kt
@@ -1,32 +1,69 @@
 package com.isidroid.a18
 
 import android.annotation.SuppressLint
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
+import android.view.View
+import androidx.core.app.ActivityOptionsCompat
+import com.bumptech.glide.Glide
+import com.isidroid.a18.sample.SamplePostsActivity
+import com.isidroid.a18.sample.SamplePostsActivity.Companion.ARG_IMAGE
+import com.isidroid.a18.sample.SamplePostsActivity.Companion.ARG_SMALL
+import com.isidroid.a18.sample.SamplePostsActivity.Companion.ARG_TEXT
 import com.isidroid.utilsmodule.BaseActivity
-import com.isidroid.utilsmodule.addTo
 import dagger.android.AndroidInjection
-import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.subjects.PublishSubject
-import timber.log.Timber
+import io.reactivex.Flowable
+import io.reactivex.schedulers.Schedulers
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+import android.opengl.ETC1.getHeight
+import android.opengl.ETC1.getWidth
 
 
 class MainActivity : BaseActivity() {
+    private val small = "https://i.redditmedia.com/231klF-0ULqETxRvq0gx61DWnsm5sE1tS7SVWZbDgLk.png?fit=crop&crop=faces%2Centropy&arh=2&w=108&s=5067eb7fc1771c87ff2f5a4ce078c4ff"
+    private val full = "https://pp.userapi.com/c834200/v834200039/178b97/AQojoR-WLx0.jpg"
+
     @SuppressLint("CheckResult")
     override fun onCreate(savedInstanceState: Bundle?) {
         AndroidInjection.inject(this)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val compositeDisposable = CompositeDisposable()
-        var subject = PublishSubject.create<Int>()
-        subject.subscribe { Timber.i("1next 1 $it") }.addTo(compositeDisposable)
+        Flowable.just("")
+                .doOnNext { Glide.get(this).clearDiskCache() }
+                .subscribeOn(Schedulers.io())
+                .subscribe()
 
-        subject.onNext(1)
-        subject.onNext(2)
-        compositeDisposable.clear()
-        subject.subscribe { Timber.i("2next $it") }.addTo(compositeDisposable)
+        Glide.with(imageview)
+                .load(full)
+                .into(imageview)
 
-        subject.onNext(3)
+        results.text = UUID.randomUUID().toString()
+        button.setOnClickListener { open() }
     }
 
+    private fun open() {
+        val intent = Intent(this, SamplePostsActivity::class.java).apply {
+            putExtra(ARG_TEXT, results.text)
+            putExtra(ARG_IMAGE, full)
+            putExtra(ARG_SMALL, small)
+        }
+
+        val p1: androidx.core.util.Pair<View, String> = androidx.core.util.Pair.create(results, "profile")
+        val p2: androidx.core.util.Pair<View, String> = androidx.core.util.Pair.create(imageview, "imageview")
+
+        var options = ActivityOptionsCompat.makeSceneTransitionAnimation(this, p1, p2)
+        options = ActivityOptionsCompat.makeBasic()
+
+//        options = ActivityOptionsCompat.makeClipRevealAnimation(
+//                imageview, 0, 0, imageview.width, imageview.height)
+
+//        options = ActivityOptionsCompat.makeThumbnailScaleUpAnimation(imageview, (imageview.drawable as BitmapDrawable).bitmap, 0, 0)
+
+
+        startActivity(intent, options.toBundle())
+    }
 }
--- app/src/main/java/com/isidroid/a18/sample/SamplePostsActivity.kt
+++ app/src/main/java/com/isidroid/a18/sample/SamplePostsActivity.kt
@@ -1,24 +1,40 @@
 package com.isidroid.a18.sample
 
 import android.annotation.SuppressLint
+import android.graphics.drawable.Drawable
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.bumptech.glide.request.target.SimpleTarget
+import com.bumptech.glide.request.transition.Transition
 import com.isidroid.a18.R
-import com.isidroid.loggermodule.Diagnostics.Companion.LOGTAG
-import com.isidroid.utilsmodule.YRealm
-import timber.log.Timber
+import com.isidroid.utilsmodule.BaseActivity
+import kotlinx.android.synthetic.main.activity_sample_posts.*
 
-class SamplePostsActivity : AppCompatActivity() {
+class SamplePostsActivity : BaseActivity() {
 
 
     @SuppressLint("CheckResult")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sample_posts)
+        supportPostponeEnterTransition()
 
-        val person = YRealm.fromJson(intent.getStringExtra("person"), Person::class.java)
-
-        Timber.tag(LOGTAG).i("SamplePostsActivity $person")
+        results.text = intent.getStringExtra(ARG_TEXT)
+        Glide.with(imageview)
+                .load(intent.getStringExtra(ARG_IMAGE))
+                .into(object : SimpleTarget<Drawable>() {
+                    override fun onResourceReady(resource: Drawable, transition: Transition<in Drawable>?) {
+                        imageview.setImageDrawable(resource)
+                        supportStartPostponedEnterTransition()
+                    }
+                })
     }
 
+    companion object {
+        const val ARG_TEXT = "ARG_TEXT"
+        const val ARG_IMAGE = "ARG_IMAGE"
+        const val ARG_SMALL = "ARG_SMALL"
+
+    }
 }
