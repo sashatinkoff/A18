1aecc8239c299eb3cb5fec2e69708613e92804c8
A. Tamakov <sashatinkoff@gmail.com>
2018-08-17 19:00:42 +0300

    Update versions. Global gradle properties

--- utilsmodule/src/main/java/com/isidroid/utilsmodule/utils/YTimeUtils.kt
+++ utilsmodule/src/main/java/com/isidroid/utilsmodule/utils/YTimeUtils.kt
@@ -5,16 +5,16 @@ import java.util.*
 
 object YTimeUtils {
     fun isSameDay(date1: Date?, date2: Date?): Boolean {
-        var date1 = date1 ?: Date()
-        var date2 = date2 ?: Date()
-        var sf = SimpleDateFormat("yyyyMMdd")
-        return sf.format(date1) == sf.format(date2)
+        val d1 = date1 ?: Date()
+        val d2 = date2 ?: Date()
+        val sf = SimpleDateFormat("yyyyMMdd")
+        return sf.format(d1) == sf.format(d2)
     }
 
     fun nextQuarterHour(now: Date = Date()): Date {
         val calendar = Calendar.getInstance().apply { time = now }
         val minute = calendar.get(Calendar.MINUTE)
-        var quoter = when (minute) {
+        val quoter = when (minute) {
             in 0..14 -> 15
             in 15..29 -> 30
             in 30..44 -> 45
