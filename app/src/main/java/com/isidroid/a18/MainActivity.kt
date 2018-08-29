35fa1ae4b19d4d8890bf53f5b51d312907b8a50c
A. Tamakov <sashatinkoff@gmail.com>
2018-08-29 11:00:40 +0500

    #15567 Remove Full Screen Icon - Add it to the menu

--- app/src/main/java/com/isidroid/a18/MainActivity.kt
+++ app/src/main/java/com/isidroid/a18/MainActivity.kt
@@ -2,12 +2,8 @@ package com.isidroid.a18
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import android.os.Handler
-import com.isidroid.loggermodule.Diagnostics.Companion.LOGTAG
-import com.isidroid.loggermodule.diagnostics
 import com.isidroid.utilsmodule.BaseActivity
 import dagger.android.AndroidInjection
-import timber.log.Timber
 
 
 class MainActivity : BaseActivity() {
@@ -16,7 +12,12 @@ class MainActivity : BaseActivity() {
         AndroidInjection.inject(this)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
 
+
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.container, DetailFragment.create(), "notags")
+            commit()
+        }
+    }
 }
 
