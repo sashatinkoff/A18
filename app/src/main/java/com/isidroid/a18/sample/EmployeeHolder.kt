f8bfca149cef9e363d977c99f548f7c96497f2b3
A. Tamakov <sashatinkoff@gmail.com>
2018-09-10 21:29:52 +0500

    test loading image in adapter by click

--- app/src/main/java/com/isidroid/a18/sample/EmployeeHolder.kt
+++ app/src/main/java/com/isidroid/a18/sample/EmployeeHolder.kt
@@ -8,7 +8,7 @@ class EmployeeHolder(b: SampleItemEmployeeBinding) : CoreBindHolder<Employee, Sa
     override fun onBind(item: Employee) {
         binding.employee = item
         binding.imageView.setOnClickListener {
-            item.age++
+            item.image = "https://cdn.shopify.com/s/files/1/0659/4707/products/PMW222.jpg?v=1527388268"
             binding.viewModel?.edit(item)
         }
 
