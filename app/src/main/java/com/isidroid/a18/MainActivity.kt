d5287ad0aa086a6bfd46033f0a4875fc99aaad04
A. Tamakov <sashatinkoff@gmail.com>
2018-09-11 10:22:00 +0500

    Clean master

--- app/src/main/java/com/isidroid/a18/MainActivity.kt
+++ app/src/main/java/com/isidroid/a18/MainActivity.kt
@@ -2,26 +2,11 @@ package com.isidroid.a18
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.SimpleItemAnimator
-import com.isidroid.a18.databinding.ActivityMainBinding
-import com.isidroid.a18.databinding.SampleItemEmployeeBinding
-import com.isidroid.a18.sample.Employee
-import com.isidroid.a18.sample.EmployeeAdapter
-import com.isidroid.a18.sample.viewmodel.EmployeesViewModel
 import com.isidroid.utilsmodule.BaseActivity
-import com.isidroid.utilsmodule.adapters.CoreBindAdapter
 import dagger.android.AndroidInjection
-import kotlinx.android.synthetic.main.activity_main.*
-import timber.log.Timber
 
 
 class MainActivity : BaseActivity() {
-    private lateinit var adapter: CoreBindAdapter<Employee, SampleItemEmployeeBinding>
-    private lateinit var viewModel: EmployeesViewModel
 
     @SuppressLint("CheckResult")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,33 +14,6 @@ class MainActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
-        viewModel = ViewModelProviders.of(this).get(EmployeesViewModel::class.java)
-
-        adapter = EmployeeAdapter().apply { this.viewModel = this@MainActivity.viewModel }
-                .create()
-                .onLoadMore { viewModel.loadMore() }
-
-        viewModel.employees.observe(this, Observer {
-            val hasMore = adapter.items.size == 0
-
-            swipeLayout.isRefreshing = false
-            adapter.insert(it, false)
-        })
-
-        viewModel.editEmployee.observe(this, Observer {
-            swipeLayout.isRefreshing = false
-            if (it.isEdit) adapter.update(it.employee)
-            else if (it.isRemove) adapter.remove(it.employee)
-        })
-
-
-        (recyclerview.itemAnimator as? SimpleItemAnimator)?.supportsChangeAnimations = false
-        recyclerview.layoutManager = LinearLayoutManager(this)
-        recyclerview.adapter = adapter
-
-        swipeLayout.setOnRefreshListener {
-            adapter.reset()
-        }
     }
 
 }
