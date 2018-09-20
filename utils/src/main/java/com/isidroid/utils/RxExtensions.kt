549a77a9fd56de383388df4e9e750a5423a9b367
Sashatinkoff <sashatinkoff@gmail.com>
2018-09-20 15:55:07 +0500

    RxExtension: subscribe to back, observe to main threads

--- utils/src/main/java/com/isidroid/utils/RxExtensions.kt
+++ utils/src/main/java/com/isidroid/utils/RxExtensions.kt
@@ -1,14 +1,24 @@
 package com.isidroid.utils
 
+import io.reactivex.Flowable
+import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.disposables.Disposable
-
-/**
- * Extension function to add a Disposable to a CompositeDisposable
- */
+import io.reactivex.schedulers.Schedulers
 
 fun Disposable.addTo(compositeDisposable: CompositeDisposable): Disposable {
     compositeDisposable.add(this)
     return this
 }
 
+fun <T> Flowable<T>.uiSubscribe(): Flowable<T> {
+    return subscribeBack().subscribeUi()
+}
+
+fun <T> Flowable<T>.subscribeBack(): Flowable<T> {
+    return subscribeOn(Schedulers.io())
+}
+
+fun <T> Flowable<T>.subscribeUi(): Flowable<T> {
+    return observeOn(AndroidSchedulers.mainThread())
+}
