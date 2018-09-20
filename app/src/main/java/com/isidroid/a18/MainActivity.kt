267c6febf5bf9238ecccf14dc232783ea249cfc1
Sashatinkoff <sashatinkoff@gmail.com>
2018-09-20 15:55:39 +0500

    clean code

--- app/src/main/java/com/isidroid/a18/MainActivity.kt
+++ app/src/main/java/com/isidroid/a18/MainActivity.kt
@@ -2,16 +2,12 @@ package com.isidroid.a18
 
 import android.os.Bundle
 import com.isidroid.utils.BaseActivity
-import com.isidroid.utils.utils.UpgradeHelper
-import timber.log.Timber
 
 
 class MainActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_main)
-        Timber.i("upgrader=${UpgradeHelper.get()}")
     }
 }
