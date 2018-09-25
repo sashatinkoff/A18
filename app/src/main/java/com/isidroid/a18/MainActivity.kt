8ff6b0f4d3cc13f440107751ad9e4a4e0f21523a
Sashatinkoff <sashatinkoff@gmail.com>
2018-09-25 16:57:27 +0500

    1

--- app/src/main/java/com/isidroid/a18/MainActivity.kt
+++ app/src/main/java/com/isidroid/a18/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.isidroid.a18
 
 import android.os.Bundle
-import com.isidroid.a18.databinding.SamplePageBinding
 import com.isidroid.utils.BaseActivity
 
 
@@ -10,10 +9,5 @@ class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        supportFragmentManager.beginTransaction().apply {
-            replace(R.id.container, SamplePage(), "sdfsdf")
-            commitAllowingStateLoss()
-        }
     }
 }
