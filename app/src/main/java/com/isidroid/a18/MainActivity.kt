6344d231e769b238c53796f6b9e8937c66ffb6fb
A. Tamakov <sashatinkoff@gmail.com>
2018-10-02 19:53:53 +0500

    clean code

--- app/src/main/java/com/isidroid/a18/MainActivity.kt
+++ app/src/main/java/com/isidroid/a18/MainActivity.kt
@@ -1,11 +1,7 @@
 package com.isidroid.a18
 
 import android.os.Bundle
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.isidroid.a18.sample.adapters.Adapter
 import com.isidroid.utils.BaseActivity
-import com.isidroid.utils.adapters.CoreBindAdapter
-import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : BaseActivity() {
@@ -13,13 +9,6 @@ class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        container.layoutManager = LinearLayoutManager(this)
-        container.adapter = Adapter()
-
-        materialButton.setOnClickListener {
-            (container.adapter as CoreBindAdapter<*>).reset()
-        }
     }
 }
 
