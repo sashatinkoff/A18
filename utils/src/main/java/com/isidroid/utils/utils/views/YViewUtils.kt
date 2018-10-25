a9161eb7c16610caf9ee275f7b172b9948b63793
Sashatinkoff <sashatinkoff@gmail.com>
2018-10-25 11:14:09 +0500

    ExtViews

--- utils/src/main/java/com/isidroid/utils/utils/views/YViewUtils.kt
+++ utils/src/main/java/com/isidroid/utils/utils/views/YViewUtils.kt
@@ -43,15 +43,3 @@ object YViewUtils {
     }
 }
 
-inline fun ViewPager.onSelected(crossinline action: (position: Int) -> Unit) = addListener(onSelected = action)
-inline fun ViewPager.addListener(crossinline onSelected: (position: Int) -> Unit = {}): ViewPager {
-
-    val listener = object : ViewPager.OnPageChangeListener {
-        override fun onPageScrollStateChanged(state: Int) {}
-        override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {}
-        override fun onPageSelected(position: Int) = onSelected(position)
-    }
-
-    addOnPageChangeListener(listener)
-    return this
-}
