a1dcdff03f35a36dbdec898e4a5fcc6e0307d28e
Sashatinkoff <sashatinkoff@gmail.com>
2018-11-19 10:14:51 +0500

    Clean code

--- app/src/main/java/com/isidroid/a18/MainActivity.kt
+++ app/src/main/java/com/isidroid/a18/MainActivity.kt
@@ -2,11 +2,9 @@ package com.isidroid.a18
 
 import android.os.Bundle
 import com.isidroid.utils.BaseActivity
-
+import com.isidroid.utils.adapters.CoreBindAdapter
 
 class MainActivity : BaseActivity() {
-    override fun onCreateViewModel() {}
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
