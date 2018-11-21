f3372d05f051025256ce0188c47b7959c2f39829
tamakov <sashatinkoff@gmail.com>
2018-11-21 11:00:01 +0500

    Bottomsheet Helper

---
+++ utils/src/main/java/com/isidroid/utils/utils/views/BottomsheetHelper.kt
@@ -0,0 +1,65 @@
+package com.isidroid.utils.utils.views
+
+import android.view.View
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetBehavior.*
+
+open class BottomsheetHelper(private val view: View) {
+    lateinit var behavior: BottomSheetBehavior<View>
+    private var onSlide: ((View, Float) -> Unit)? = null
+    private var onStateChanged: ((View, Int) -> Unit)? = null
+    private var onCollapsed: ((View, Int) -> Unit)? = null
+    private var onExpanded: ((View, Int) -> Unit)? = null
+    private var onHidden: ((View, Int) -> Unit)? = null
+    private var onHalfExpanded: ((View, Int) -> Unit)? = null
+
+    fun onSlide(callback: (view: View, offset: Float) -> Unit) = apply { this.onSlide = callback }
+    fun onStateChanged(callback: (view: View, state: Int) -> Unit) = apply { this.onStateChanged = callback }
+    fun onCollapsed(callback: (view: View, state: Int) -> Unit) = apply { this.onCollapsed = callback }
+    fun onExpanded(callback: (view: View, state: Int) -> Unit) = apply { this.onExpanded = callback }
+    fun onHidden(callback: (view: View, state: Int) -> Unit) = apply { this.onHidden = callback }
+    fun onHalfExpanded(callback: (view: View, state: Int) -> Unit) = apply { this.onHalfExpanded = callback }
+
+    private val bottomSheetCallback = object : BottomSheetBehavior.BottomSheetCallback() {
+        override fun onSlide(view: View, offset: Float) {
+            onSlide?.invoke(view, offset)
+        }
+
+        override fun onStateChanged(view: View, state: Int) {
+            onStateChanged?.invoke(view, state)
+            when (state) {
+                STATE_COLLAPSED -> onCollapsed?.invoke(view, state)
+                STATE_EXPANDED -> onExpanded?.invoke(view, state)
+                STATE_HIDDEN -> onHidden?.invoke(view, state)
+                STATE_HALF_EXPANDED -> onHalfExpanded?.invoke(view, state)
+                else -> {
+                }
+            }
+        }
+    }
+
+    fun create() = apply {
+        behavior = BottomSheetBehavior.from(view)
+        behavior.setBottomSheetCallback(bottomSheetCallback)
+    }
+
+    open fun onStateChanged(view: View, state: Int) {}
+
+    fun expand() = apply { behavior.state = STATE_EXPANDED }
+    fun collapse() = apply { behavior.state = STATE_COLLAPSED }
+    fun hide() = apply { behavior.state = STATE_HIDDEN }
+
+    fun isExpanded() = behavior.state == STATE_EXPANDED
+    fun isCollapsed() = behavior.state == STATE_COLLAPSED
+    fun isHidden() = behavior.state == STATE_HIDDEN
+
+    fun state(state: Int): String {
+        return when (state) {
+            STATE_COLLAPSED -> "STATE_COLLAPSED"
+            STATE_EXPANDED -> "STATE_EXPANDED"
+            STATE_HIDDEN -> "STATE_HIDDEN"
+            STATE_HALF_EXPANDED -> "STATE_HALF_EXPANDED"
+            else -> ""
+        }
+    }
+}
