7842ab29f7674e2de4558b72c0f5403344a064a0
A. Tamakov <sashatinkoff@gmail.com>
2018-12-09 21:10:50 +0500

    1

--- app/src/main/java/com/isidroid/a18/MainActivity.kt
+++ app/src/main/java/com/isidroid/a18/MainActivity.kt
@@ -1,68 +1,82 @@
 package com.isidroid.a18
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.app.Service
 import android.content.Context
+import android.content.Intent
 import android.net.ConnectivityManager
 import android.net.Network
 import android.os.Bundle
 import com.isidroid.utils.BaseActivity
 import timber.log.Timber
 import android.net.NetworkRequest
+import android.os.Handler
+import android.os.IBinder
+import android.os.Looper
 import androidx.core.content.ContextCompat.getSystemService
+import com.google.android.gms.location.*
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.listener.PermissionGrantedResponse
+import com.karumi.dexter.listener.single.CompositePermissionListener
 import kotlinx.android.synthetic.main.activity_main.*
 import java.lang.Exception
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 class MainActivity : BaseActivity() {
-    var isConnected = false
-    private lateinit var manager: ConnectivityManager
-    private lateinit var nc: NC
-    val networkRequest = NetworkRequest.Builder().build()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        manager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        nc = NC(manager)
 
-        button.setOnClickListener { execute() }
+        btnStart.setOnClickListener { startService(Intent(this, YService::class.java)) }
+        btnStop.setOnClickListener {
+            startService(Intent(this, YService::class.java)
+                    .setAction("STOP"))
+        }
     }
 
-    private fun execute() {
-        try {
-            if (!isConnected) {
-                manager.registerNetworkCallback(networkRequest, NC(manager))
-            } else {
-                manager.unregisterNetworkCallback(nc)
-            }
-        } catch (e: Exception) {
-            Timber.e(e)
-        }
 
-        isConnected = !isConnected
+}
+
+class YService : Service() {
+    lateinit var gpsClient: FusedLocationProviderClient
+
+    val locationRequest = LocationRequest.create().apply {
+        interval = (1000 * 3 * 1)
+        fastestInterval = interval / 2
+        priority = LocationRequest.PRIORITY_HIGH_ACCURACY
     }
 
-    class NC(private val manager: ConnectivityManager) : ConnectivityManager.NetworkCallback() {
-        override fun onLost(network: Network) {
-            debug("onLost")
+    val locationCallback = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult?) {
+            Timber.e("onLocationResult $locationResult")
         }
+    }
 
-        override fun onLosing(network: Network, maxMsToLive: Int) {
-            debug("onLosing")
-        }
+    override fun onBind(p0: Intent?): IBinder? {
+        return null
+    }
 
-        override fun onAvailable(network: Network) {
-            debug("onAvailable")
-        }
+    @SuppressLint("MissingPermission")
+    override fun onCreate() {
+        super.onCreate()
 
-        override fun onUnavailable() {
-            debug("onUnavailable")
-        }
+        gpsClient = LocationServices.getFusedLocationProviderClient(this)
+        gpsClient.requestLocationUpdates(locationRequest, locationCallback, Looper.getMainLooper())
+    }
 
-        private fun debug(type: String) {
-            Timber.tag("NetworkCallback").i("$type " +
-                    "${manager.activeNetworkInfo?.extraInfo}, " +
-                    "${manager.activeNetworkInfo?.detailedState}")
-        }
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        if (intent?.action == "STOP") stopSelf()
+
+        return START_STICKY
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        gpsClient.removeLocationUpdates(locationCallback)
+        Timber.e("onDestroy")
     }
 }
