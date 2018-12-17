5655cb08e5239836fb30f5e8931624a9db597c66
A. Tamakov <sashatinkoff@gmail.com>
2018-12-17 11:51:26 +0500

    Temporary solution for OOM for HUGEST bitmaps

--- app/src/main/java/com/isidroid/a18/MainActivity.kt
+++ app/src/main/java/com/isidroid/a18/MainActivity.kt
@@ -1,28 +1,13 @@
 package com.isidroid.a18
 
-import android.Manifest
-import android.annotation.SuppressLint
-import android.app.Service
-import android.content.Context
-import android.content.Intent
-import android.net.ConnectivityManager
-import android.net.Network
+import android.graphics.Bitmap
 import android.os.Bundle
 import com.isidroid.utils.BaseActivity
-import timber.log.Timber
-import android.net.NetworkRequest
-import android.os.Handler
-import android.os.IBinder
-import android.os.Looper
-import androidx.core.content.ContextCompat.getSystemService
-import com.google.android.gms.location.*
-import com.karumi.dexter.Dexter
-import com.karumi.dexter.listener.PermissionGrantedResponse
-import com.karumi.dexter.listener.single.CompositePermissionListener
+import com.isidroid.utils.addTo
+import io.reactivex.Flowable
+import io.reactivex.disposables.CompositeDisposable
 import kotlinx.android.synthetic.main.activity_main.*
-import java.lang.Exception
-import java.text.SimpleDateFormat
-import java.util.*
+import timber.log.Timber
 
 
 class MainActivity : BaseActivity() {
@@ -30,53 +15,30 @@ class MainActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-        btnStart.setOnClickListener { startService(Intent(this, YService::class.java)) }
-        btnStop.setOnClickListener {
-            startService(Intent(this, YService::class.java)
-                    .setAction("STOP"))
-        }
+        btnStart.setOnClickListener { execute() }
     }
 
+    private fun execute() {
+        val data = arrayListOf(1, 200, 400, 50000)
+        val updated = mutableListOf("Some string")
+        val upload = Flowable.just("COMPLETE ACTION")
 
-}
+        Flowable.concat(Flowable.fromIterable(data), Flowable.fromIterable(updated), upload)
+                .map { param ->
+                    var result = ""
 
-class YService : Service() {
-    lateinit var gpsClient: FusedLocationProviderClient
+                    (param as? Int)?.apply {
+                        val bitmap = Bitmap.createBitmap(param, param, Bitmap.Config.ARGB_8888)
+                        result = "create bitmap " +
+//                                "[${bitmap.width}, ${bitmap.height}]" +
+                                ""
 
-    val locationRequest = LocationRequest.create().apply {
-        interval = (1000 * 3 * 1)
-        fastestInterval = interval / 2
-        priority = LocationRequest.PRIORITY_HIGH_ACCURACY
-    }
+                    }
 
-    val locationCallback = object : LocationCallback() {
-        override fun onLocationResult(locationResult: LocationResult?) {
-            Timber.e("onLocationResult $locationResult")
-        }
-    }
-
-    override fun onBind(p0: Intent?): IBinder? {
-        return null
-    }
-
-    @SuppressLint("MissingPermission")
-    override fun onCreate() {
-        super.onCreate()
-
-        gpsClient = LocationServices.getFusedLocationProviderClient(this)
-        gpsClient.requestLocationUpdates(locationRequest, locationCallback, Looper.getMainLooper())
-    }
-
-    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        if (intent?.action == "STOP") stopSelf()
-
-        return START_STICKY
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        gpsClient.removeLocationUpdates(locationCallback)
-        Timber.e("onDestroy")
+                    (param as? String)?.apply { result = param }
+                    result
+                }
+                .subscribe({ Timber.i("next $it") }, { Timber.e(it) }, { Timber.e("complete") })
+                .addTo(CompositeDisposable())
     }
 }
