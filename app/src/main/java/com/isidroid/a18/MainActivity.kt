faa87987fd55be71268344f9db84d32b146c183d
A. Tamakov <sashatinkoff@gmail.com>
2019-01-04 16:05:33 +0500

    Release 1.1.53

--- app/src/main/java/com/isidroid/a18/MainActivity.kt
+++ app/src/main/java/com/isidroid/a18/MainActivity.kt
@@ -1,27 +1,19 @@
 package com.isidroid.a18
 
-import android.Manifest
 import android.os.Bundle
-import com.isidroid.utils.BaseActivity
-import com.karumi.dexter.Dexter
-import com.karumi.dexter.listener.single.CompositePermissionListener
+import com.isidroid.a18.creditcard.CreditCardTextWatcher
+import com.isidroid.a18.creditcard.CreditCardUtil
+import com.isidroid.a18.databinding.ActivityMainBinding
+import com.isidroid.utils.BindActivity
 import kotlinx.android.synthetic.main.activity_main.*
-import timber.log.Timber
 
 
-class MainActivity : BaseActivity() {
+class MainActivity : BindActivity<ActivityMainBinding>() {
+    override val resource = R.layout.activity_main
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        Dexter.withActivity(this).withPermission(Manifest.permission.ACCESS_FINE_LOCATION)
-                .withListener(CompositePermissionListener()).check()
-
-        btnRead.setOnClickListener { GpsService.start(this) }
-        btnCar.setOnClickListener { GpsService.stop(this) }
-
-        GpsClient.connect(this)
-                .onLocationChanged { Timber.i("onLocationChanged ${javaClass.simpleName} $it") }
-                .disconnect()
+        CreditCardTextWatcher(creditCardInput) { type -> textview.text = "${type.name}, length=${CreditCardUtil.cvvLength(type)}" }
     }
 }
