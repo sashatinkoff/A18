efe57069025762026b55175a6bf21048df796c77
A. Tamakov <sashatinkoff@gmail.com>
2019-01-04 10:00:54 +0500

    Extended functionality for views

--- utils/src/main/java/com/isidroid/utils/utils/views/ExtViews.kt
+++ utils/src/main/java/com/isidroid/utils/utils/views/ExtViews.kt
@@ -1,9 +1,17 @@
 package com.isidroid.utils.utils.views
 
+import android.animation.Animator
+import android.animation.ValueAnimator
 import android.text.Editable
 import android.text.TextWatcher
+import android.view.View
+import android.view.ViewGroup
+import android.view.animation.DecelerateInterpolator
+import android.widget.TextView
 import androidx.appcompat.widget.AppCompatEditText
 import androidx.viewpager.widget.ViewPager
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.snackbar.SnackbarContentLayout
 
 inline fun ViewPager.onSelected(crossinline action: (position: Int) -> Unit) = addListener(onSelected = action)
 inline fun ViewPager.addListener(crossinline onSelected: (position: Int) -> Unit = {}): ViewPager {
@@ -33,4 +41,45 @@ inline fun AppCompatEditText.addTextWatcherListener(
     }
 
     addTextChangedListener(listener)
+}
+
+fun View.expand(duration: Long, targetHeight: Int) = apply {
+    visibility = View.VISIBLE
+    val valueAnimator = ValueAnimator.ofInt(height, targetHeight)
+    valueAnimator.addUpdateListener { animation ->
+        layoutParams.height = animation.animatedValue as Int
+        requestLayout()
+    }
+    valueAnimator.interpolator = DecelerateInterpolator()
+    valueAnimator.duration = duration
+    valueAnimator.start()
+}
+
+fun View.collapse(duration: Long) = apply {
+    val prevHeight = height
+    val valueAnimator = ValueAnimator.ofInt(prevHeight, 0)
+    valueAnimator.interpolator = DecelerateInterpolator()
+    valueAnimator.addUpdateListener { animation ->
+        layoutParams.height = animation.animatedValue as Int
+        requestLayout()
+    }
+    valueAnimator.addListener(object : Animator.AnimatorListener {
+        override fun onAnimationEnd(animation: Animator?) {
+            visibility = View.GONE
+        }
+
+        override fun onAnimationRepeat(animation: Animator?) {}
+        override fun onAnimationCancel(animation: Animator?) {}
+        override fun onAnimationStart(animation: Animator?) {}
+
+    })
+    valueAnimator.interpolator = DecelerateInterpolator()
+    valueAnimator.duration = duration
+    valueAnimator.start()
+}
+
+fun Snackbar.setTextColor(color: Int): Snackbar {
+    val contentLayout = (view as ViewGroup).getChildAt(0) as? SnackbarContentLayout
+    contentLayout?.findViewById<TextView>(com.google.android.material.R.id.snackbar_text)?.setTextColor(color)
+    return this
 }
