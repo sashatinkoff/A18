bc02539050e14a204877739409502ccf8f4b437a
A. Tamakov <sashatinkoff@gmail.com>
2019-01-11 14:05:46 +0500

    test

--- app/src/main/java/com/isidroid/a18/MainActivity.kt
+++ app/src/main/java/com/isidroid/a18/MainActivity.kt
@@ -1,29 +1,47 @@
 package com.isidroid.a18
 
+import android.Manifest
+import android.content.Intent
 import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import com.isidroid.a18.databinding.ActivityMainBinding
-import com.isidroid.a18.queues.QueueManager
+import com.isidroid.pics.viewmodel.TakePictureViewModel
 import com.isidroid.utils.BindActivity
-import io.reactivex.disposables.CompositeDisposable
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.listener.single.CompositePermissionListener
 import kotlinx.android.synthetic.main.activity_main.*
+import timber.log.Timber
 
 
 class MainActivity : BindActivity<ActivityMainBinding>() {
     override val resource = R.layout.activity_main
-    private val manager = QueueManager<String>(CompositeDisposable())
-    private var counter = 0
+    private lateinit var takepictureViewModel: TakePictureViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-//        btnSubmit.setOnClickListener {
-//            counter++
-//            manager.add(Status(counter))
-//        }
-//
-//        btnCancel.setOnClickListener {
-//            manager.cancel(counter - 1)
-//        }
+        Dexter.withActivity(this).withPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE).withListener(CompositePermissionListener()).check()
+
+        btnSubmit.setOnClickListener {
+            takepictureViewModel.pickGallery(this)
+        }
     }
+
+    override fun onCreateViewModel() {
+        takepictureViewModel = ViewModelProviders.of(this).get(TakePictureViewModel::class.java)
+        takepictureViewModel.error.observe(this, Observer {
+            Timber.e(it)
+        })
+        takepictureViewModel.imageInfo.observe(this, Observer {
+            imageview.setImageBitmap(it.result?.bitmap)
+        })
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (resultCode != RESULT_OK) return
+        takepictureViewModel.onResult(requestCode, data)
+    }
+
 
 }
