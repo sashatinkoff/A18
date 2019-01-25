638cbd8618d1e59219f5c46697f9fc2b7688d119
A. Tamakov <sashatinkoff@gmail.com>
2019-01-25 12:12:58 +0500

    1

--- app/src/main/java/com/isidroid/a18/MainActivity.kt
+++ app/src/main/java/com/isidroid/a18/MainActivity.kt
@@ -3,6 +3,7 @@ package com.isidroid.a18
 import android.os.Bundle
 import com.isidroid.a18.databinding.ActivityMainBinding
 import com.isidroid.utils.BindActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : BindActivity<ActivityMainBinding>() {
@@ -10,5 +11,7 @@ class MainActivity : BindActivity<ActivityMainBinding>() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        btnOpen.setOnClickListener { createBottomsheet(bottomSheet, coordinator).expand() }
     }
 }
