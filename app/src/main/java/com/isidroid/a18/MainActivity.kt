5acf28236784b67b634c4105a907e91f88e4e4a2
A. Tamakov <sashatinkoff@gmail.com>
2019-04-10 10:45:51 +0500

    Clean code

--- app/src/main/java/com/isidroid/a18/MainActivity.kt
+++ app/src/main/java/com/isidroid/a18/MainActivity.kt
@@ -28,13 +28,10 @@ import java.util.concurrent.TimeUnit
 
 
 class MainActivity : BindActivity<ActivityMainBinding>() {
-    private lateinit var viewmodel: TakePictureViewModel
-
     override val resource = R.layout.activity_main
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        if (intent?.action == Intent.ACTION_SEND || intent?.action == Intent.ACTION_SEND_MULTIPLE) handleSendData(intent)
         Dexter.withActivity(this).withPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
             .withListener(CompositePermissionListener()).check()
 
@@ -44,104 +41,10 @@ class MainActivity : BindActivity<ActivityMainBinding>() {
             createBottomsheet(bottomSheet, coordinator) { it?.alpha(.5f) }.expand()
         }
 
-        btnSave.setOnClickListener { Timber.i("click on btnsave") }
-        btnPdf.setOnClickListener {
-            Timber.i("click on btnPdf")
-            try {
-                throw Exception("click throwed an exception")
-            } catch (e: Exception) {
-                Timber.e(e)
-            }
-
-        }
-
-        btnCamera.setOnClickListener {
-            Diagnostics.instance.getShareLogsIntent(this, true)
-                .subscribeIoMain()
-                .subscribe { startActivity(it) }
-        }
-
-
-        btnSave.setOnClickListener { viewmodel.pickGallery(this, true) }
-        btnPdf.setOnClickListener { viewmodel.pick(this, "application/pdf") }
-        btnCamera.setOnClickListener {
-            viewmodel.takePicture(this)
-        }
-
-//        btnSave.setOnClickListener {
-//            AlertDialog.Builder(this)
-//                .setTitle("Greetings to you")
-//                .setMessage("Hello, this is just a message with several lines\nWith the best wishes, just me")
-//                .setPositiveButton("Positive", null)
-//                .setNegativeButton("Negative", null)
-//                .setNeutralButton("Neutral", null)
-//                .show()
-//        }
-//
-//        btnCamera.setOnClickListener {
-//            val items = arrayListOf("First", "Second", "Last").toTypedArray()
-//            AlertDialog.Builder(this)
-//                .setTitle("Greetings to you")
-//                .setMultiChoiceItems(items, null, null)
-//                .setPositiveButton("Positive", null)
-//                .setNegativeButton("Negative", null)
-//                .setNeutralButton("Neutral", null)
-//                .show()
-//        }
-
-
-        // show simple dialog with text
-//        AlertDialog.Builder(this)
-//            .setTitle("Greetings to you")
-//            .setMessage("Hello, this is just a message with several lines\nWith the best wishes, just me")
-//            .setPositiveButton("Positive", null)
-//            .setNegativeButton("Negative", null)
-//            .setNeutralButton("Neutral", null)
-//            .show()
-//
-//        // show dialog with multiple checkboxes
-//        val items = arrayListOf("First", "Second", "Last").toTypedArray()
-//        AlertDialog.Builder(this)
-//            .setTitle("Greetings to you")
-//            .setMultiChoiceItems(items, null, null)
-//            .setPositiveButton("Positive", null)
-//            .setNegativeButton("Negative", null)
-//            .setNeutralButton("Neutral", null)
-//            .show()
+        btnSave.setOnClickListener { }
+        btnPdf.setOnClickListener { }
     }
 
-    private fun fake() {
-        Handler().postDelayed({
-            Timber.i("something happedn fake")
-            fake()
-        }, 1200)
-    }
-
-    override fun onCreateViewModel() {
-        viewmodel = ViewModelProviders.of(this).get(TakePictureViewModel::class.java).create(this)
-        viewmodel.error.observe(this, Observer {
-            Timber.e("IMAGE INFO OBSERVED ERROR ${it.message}")
-            Toast.makeText(this, it.message, Toast.LENGTH_SHORT).show()
-        })
-        viewmodel.imageInfo.observe(this, Observer { info ->
-            info.result?.forEach {
-                val file = File(it.localPath)
-                Timber.tag("imageinforesult").i("${file.absolutePath}/${file.exists()}")
-                Glide.with(imageview).load(it.localPath).into(imageview)
-            }
-        })
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        viewmodel.onResult(requestCode, data)
-    }
-
-    override fun onNewIntent(intent: Intent?) {
-        super.onNewIntent(intent)
-        if (intent?.action == Intent.ACTION_SEND || intent?.action == Intent.ACTION_SEND_MULTIPLE) handleSendData(intent)
-    }
-
-    private fun handleSendData(intent: Intent) {
-        viewmodel.onResult(PictureConfig.get().codePick, intent)
-    }
+    override fun onCreateViewModel() {}
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {}
 }
