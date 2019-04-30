2e86e422f3f92a5dd6c29ce96e6c56b8c26ac7c6
A. Tamakov <sashatinkoff@gmail.com>
2019-04-30 11:52:52 +0500

    bump version up

--- app/src/main/java/com/isidroid/a18/MainActivity.kt
+++ app/src/main/java/com/isidroid/a18/MainActivity.kt
@@ -1,26 +1,13 @@
 package com.isidroid.a18
 
 import android.Manifest
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
-import android.graphics.Color
 import android.os.Bundle
-import android.os.Environment
-import android.widget.Toast
-import com.google.zxing.*
-import com.google.zxing.common.HybridBinarizer
-import com.google.zxing.qrcode.QRCodeWriter
 import com.isidroid.a18.databinding.ActivityMainBinding
-import com.isidroid.logger.Diagnostics
-import com.isidroid.logger.FileLogger
 import com.isidroid.utils.BindActivity
 import com.karumi.dexter.Dexter
 import com.karumi.dexter.listener.single.CompositePermissionListener
 import kotlinx.android.synthetic.main.sample_main.*
-import org.json.JSONObject
 import timber.log.Timber
-import java.io.File
-import java.util.*
 
 
 class MainActivity : BindActivity<ActivityMainBinding>() {
